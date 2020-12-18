0a81796df5efd8bfbb727c6b89e83b49cab2cbb8
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-12-18 15:34:21 -0700

    Code added to update the website and table display.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,20 @@
-// from data.js
-var tableData = data;
-
 // YOUR CODE HERE!
+// data is from the data.js file
+
+function uploadData(data){
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    data.forEach(ufoData=> {
+        var row = tbody.append("tr");
+        Object.values(ufoData).forEach((value) => {
+            var cell = row.append("td");
+            cell.text(value);
+          });
+    });
+};
+
 // Select the button
+// uploadData(data)
 var button = d3.select("#filter-btn");
 
 // Select the form
@@ -11,10 +23,10 @@ var form = d3.select("#form");
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
-
 // Complete the event handler function for the form
 function runEnter() {
-
+  $("#tbody").empty();
+  
   // Prevent the page from refreshing
   d3.event.preventDefault();
   // Select the input element and get the raw HTML node
@@ -25,20 +37,11 @@ function runEnter() {
   console.log(inputDate);
   // Use the form input to filter the data by date
   
-  var days_data = tableData.filter(day_data => day_data.datetime === inputDate);
+  var days_data = data.filter(day_data => day_data.datetime === inputDate);
   console.log(days_data);
-  //Gather the data from the days_data to be dislayed on in the table
-  var tbody = d3.select("tbody");
-  days_data.forEach((days_data) => {
-     // Display the data in the table on the website. 
-    var row = tbody.append("tr");
-    Object.entries(days_data).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-  
 
+uploadData(days_data)  
+$("#datetime").val('')
 
 
 };
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,47 +1,62 @@
 // from data.js
-var tableData = data;
+// var tableData = data;
 
 // YOUR CODE HERE!
+// data is from data.js
+// function to add the data into the table
+function uploadData(data){
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    data.forEach(ufoData=> {
+        var row = tbody.append("tr");
+        Object.values(ufoData).forEach((value) => {
+            var cell = row.append("td");
+            cell.text(value);
+          });
+    });
+};
+
 // Select the button
-var button = d3.select("#button-btn");
+// uploadData(data)
+var button = d3.select("#filter-btn");
 
 // Select the form
 var form = d3.select("#form");
 
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+d3.selectAll("input").on("change", runEnter);
+// form.on("submit",runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
-
+  $("#tbody").empty();
   // Prevent the page from refreshing
   d3.event.preventDefault();
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputDate = d3.select("#datetime").property("value");
+  var inputCity = d3.select("#city").property("value");
+  var inputState = d3.select("#state").property("value");
+  var inputCountry = d3.select("#country").property("value");
+  var inputShape = d3.select("#shape").property("value");
 
-  // Get the value property of the input element
-  var inputDate = inputElement.property("value");
-  console.log(inputDate);
   // Use the form input to filter the data by date
+  if (inputDate){
+    days_data = data.filter(day_data => day_data.datetime === inputDate);
+  console.log(days_data)}
+  if (inputCity){
+    days_data = data.filter(city_data => city_data.city === inputCity);
+    console.log(days_data)}
+  if (inputState){
+    days_data = data.filter(state_data => state_data.state === inputState);
+  }
+  if (inputCountry){
+    days_data = data.filter(country_data => country_data.country === inputCountry);
+  }
+  if (inputShape){
+    days_data = data.filter(shape_data => shape_data.shape === inputShape);
+  }
+
+uploadData(days_data);
   
-  var days_data = tableData.filter(day_data => day_data.datetime === inputDate);
-  console.log(days_data);
-  //Gather the data from the days_data to be dislayed on in the table
-  var tbody = d3.select("tbody");
-  days_data.forEach((days_data) => {
-     // Display the data in the table on the website. 
-    var row = tbody.append("tr");
-    Object.entries(days_data).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-
-
-
-
-
-
-
 };
